d3a063c4d7c2cf4be0ee18af725510626f5b8334
srue1834 <53914141+srue1834@users.noreply.github.com>
2023-09-05 16:38:08 -0600

    clean y piano en main

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def getDrumPart():
   # -------------------------------------------------------------------------------- #
 
   drumsPart = Part("Drums", 0, 9)
+
+
   stickDrumPhrase = Phrase(0.0) # Create phrase at beat 0.0
   snareDrumPhrase = Phrase(0.0)
   cymbalDrumPhrase = Phrase(0.0)
@@ -305,6 +307,82 @@ def getBassPart():
 
   return bassPart
 
+def getCleanGuitarPart():
+   cleanGuitarPart = Part("Clean Guitar", 27, 0)
+   cleanPhrase = Phrase()
+   cleanSoloPhrase = Phrase()
+   cleanIndividualPhrase = Phrase() 
+  
+   for p in [cleanPhrase, cleanIndividualPhrase]:
+      p.setDynamic(F)
+      p.setTempo(123)
+    
+   cleanSoloPhrase.setDynamic(FF)
+   cleanSoloPhrase.setTempo(123)
+   # Intro
+   cleanNotes = [F4, B4, F4, B4, F4, B4, G4, G4, C4, G4, C4, G4, C4, G4, G4, D4, G4, D4, G4, D4, E4, E4, B4, E4, B4, E4, B4, E4] * 2
+   cleanNotesDurations = [EN, EN, EN, QN, EN, EN, EN] * 8
+   
+   # Añadir silencios y notas
+   cleanPhrase.addNoteList([REST] * 9 + cleanNotes * 3, [WN] * 9 + cleanNotesDurations * 3)
+   
+   # Añadir el tema al Part
+   cleanGuitarPart.addPhrase(cleanPhrase)
+   
+   # Bridge + Gtr. Solo 1
+   chords_pitches = [[REST]] * 8
+   chords_durations = [WN] * 8
+   
+   DURATIONS1 = [QN, EN, EN, EN, EN, EN, EN]
+   DURATIONS2 = [EN, EN, EN, QN, EN, EN, EN]
+   
+   # Definición de acordes
+   CHORDS_1_1 = [[E4, E4, C4, G4, C4], [E4, E4, C4, G4, C4], [REST], [E4, C4, G4, C4], [E4, E4, C4, G4], [E4, C4, G4, C4], [F4, C4, A4, F4, C4, F4]]
+   
+   CHORDS_1_2 = [[F4, C4, A4, F4, C4, F4], [F4, C4, A4, F4, C4, F4], [REST], [F4, C4, A4, F4, C4, F4], [C4, A4, F4, C4, F4], [F4, C4, A4, F4, C4], [C4, A4, F4, C4, F4]]
+   
+   CHORDS_1_3 = [[G4, D4, A4, G4, D4], [G4, D4, A4, G4, D4], [REST], [D4, A4, G4, D4], [G4, D4, A4, G4], [D4, A4, G4, D4], [G4, D4, B4, G4, D4, G4]]
+   
+   CHORDS_1_4 = [[G4, D4, B4, G4, D4, G4], [G4, D4, B4, G4, D4, G4], [REST], [G4, D4, B4, G4, D4, G4], [D4, B4, G4, D4, G4], [G4, D4, B4, G4, D4], [D4, B4, G4, D4, G4]]
+   
+   CHORDS_1_5 = [[E4, C4, A4, E4, A4], [E4, C4, A4, E4, A4], [REST], [C4, A4, E4, A4], [E4, C4, A4, E4], [C4, A4, E4, A4], [E4, B4, G4, E4, B4, E4]]
+   
+   CHORDS_1_6 = [[E4, B4, G4, E4, B4, E4], [E4, B4, G4, E4, B4, E4], [REST], [E4, B4, G4, E4, B4, E4], [B4, G4, E4, B4, E4], [E4, B4, G4, E4, B4], [B4, G4, E4, B4, E4]]
+   
+   CHORDS_1_7 = [[E4, C4, A4, E4, A4], [E4, C4, A4, E4, A4], [REST], [C4, A4, E4, A4], [E4, C4, A4, E4], [C4, A4, E4, A4], [G4, D4, A4, G4, D4]]
+   
+   CHORDS_1_8 = [[G4, D4, A4, G4, D4], [G4, D4, A4, G4, D4], [REST], [G4, D4, A4, G4, D4], [D4, A4, G4, D4], [G4, D4, A4, G4], [D4, A4, G4, D4]]
+   
+   # Añadir acordes y duraciones
+   add_chords(CHORDS_1_1, DURATIONS1, chords_pitches, chords_durations)
+   add_chords(CHORDS_1_2, DURATIONS2, chords_pitches, chords_durations)
+   
+   add_chords(CHORDS_1_3, DURATIONS1, chords_pitches, chords_durations)
+   add_chords(CHORDS_1_4, DURATIONS2, chords_pitches, chords_durations)
+   
+   add_chords(CHORDS_1_5, DURATIONS1, chords_pitches, chords_durations)
+   add_chords(CHORDS_1_6, DURATIONS2, chords_pitches, chords_durations)
+   
+   add_chords(CHORDS_1_7, DURATIONS1, chords_pitches, chords_durations)
+   add_chords(CHORDS_1_8, DURATIONS2, chords_pitches, chords_durations)
+   
+   
+   for pitches, duration in zip(chords_pitches, chords_durations):
+       cleanSoloPhrase.addChord(pitches, duration)
+   
+   cleanGuitarPart.addPhrase(cleanSoloPhrase)
+   
+   # Bridge p.2a
+   bridgeNotes = [B4, G4, D4, B4, G4, D4, D4, A4, F4, D4, A4, F4, D4, D4, B4, G4, E4, B4, G4, E4, E4, F4, D4, B4, F4, E4, F4, E4]
+   bridgeNotesDurations = [EN, EN, QN, EN, EN, EN, EN] * 3 + [EN, EN, EN, EN, QN, EN, EN]
+   
+   cleanIndividualPhrase.addNoteList(bridgeNotes * 4, bridgeNotesDurations * 4)
+   cleanGuitarPart.addPhrase(cleanIndividualPhrase)
+   
+   return cleanGuitarPart
+       
+  
+
 def getOverdrivenGuitarPart():
 
   # TODO This is only for testing instrument individually. Delete after finishing part.
@@ -488,16 +566,45 @@ def getOverdrivenGuitarPart():
   
   return overDrivePart
 
+def getPianoPart():
+   pianoPart = Part("Piano", 0, 0)
+   pianoPhrase = Phrase()   
+   PianoPitches0   = [D5, B4, F4, D5, E5, E5, C5, G4, E5, D5, C5, D5, B4, G4, D5, E5, E5, B4, G4, E5, D5, C5]
+   PianoDurations0 = [QN, QN, QN, EN, EN, EN, QN, QN, EN, EN, EN] * 2
+   
+   PianoPitches1 = [REST] * 5 + PianoPitches0 * 3
+   PianoDurations1 = [WN] * 5 + PianoDurations0 * 3
+   
+   # create an empty phrase, and construct theme from the above motifs
+   
+   pianoPhrase.addNoteList(PianoPitches1, PianoDurations1)
+   pianoPart.addPhrase(pianoPhrase)
+   
+   return pianoPart
+   
+   
+
+def add_chords(pitches_list, durations_list, chords_pitches, chords_durations):
+    """Función para añadir acordes y duraciones a las listas."""
+    for pitches, duration in zip(pitches_list, durations_list):
+        chords_pitches.append(pitches)
+        chords_durations.append(duration)
+
 score = Score("Song", 123.0)
 
 drumsPart = getDrumPart()
 bassPart = getBassPart()
 overdrivenPart = getOverdrivenGuitarPart()
+cleanGuitarPart = getCleanGuitarPart()
+pianoPart = getPianoPart()
 
 score.addPart(drumsPart)
 score.addPart(bassPart)
 score.addPart(overdrivenPart)
+score.addPart(cleanGuitarPart)
+score.addPart(pianoPart)
 
+View.sketch(cleanGuitarPart)
 Write.midi(score, "Drum.mid")
 Play.midi(score)
 
