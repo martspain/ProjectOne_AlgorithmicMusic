30decf59b613c4467d503cb6bd5977c62c47a7d6
srue1834 <53914141+srue1834@users.noreply.github.com>
2023-09-05 15:57:00 -0600

    clean guitar done

--- cleanGuitar.py
+++ cleanGuitar.py
@@ -1,71 +1,88 @@
 from music import *
 
-# Funcion para exportar la parte (instrumento) que nos toca
-def getDrumPart():
+def add_chords(pitches_list, durations_list, chords_pitches, chords_durations):
+    """Función para añadir acordes y duraciones a las listas."""
+    for pitches, duration in zip(pitches_list, durations_list):
+        chords_pitches.append(pitches)
+        chords_durations.append(duration)
 
-  # TODO This is only for testing instrument individually. Delete after finishing part.
-  score = Score("Drum Machine Pattern #1", 145.0)
+# Inicialización y configuración
+theme = Phrase()
+phrase = Phrase()
+individual_phrase = Phrase()
 
-  drumsPart = Part("CleanGuitar", 0, 4)
-  bassDrumPhrase = Phrase(0.0) # Create phrase at beat 0.0
+CLEAN_GUITAR_INSTRUMENT_NUMBER = 27
+MIDI_CHANNEL = 0
+cleanGuitarPart = Part(CLEAN_GUITAR_INSTRUMENT_NUMBER, MIDI_CHANNEL)
 
-  bassDrumPhrase.setInstrument(OVERDRIVEN_GUITAR)
-  bassDrumPhrase.setTempo(123)
+# Establecer dinámica y tempo para todas las frases
+for p in [theme, individual_phrase]:
+    p.setDynamic(F)
+    p.setTempo(123)
+ 
+phrase.setDynamic(FF)
+phrase.setTempo(123)
 
-  # Intro 20 4
-  bassPitches   = [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST]
-  bassDurations = [QN, EN, EN, HN, QN, EN, EN, EN, DHN] # 20 WN
+# Intro
+cleanNotes = [F4, B4, F4, B4, F4, B4, G4, G4, C4, G4, C4, G4, C4, G4, G4, D4, G4, D4, G4, D4, E4, E4, B4, E4, B4, E4, B4, E4] * 2
+cleanNotesDurations = [EN, EN, EN, QN, EN, EN, EN] * 8
 
-  # Verse1 16 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2 # 16 wn
+# Añadir silencios y notas
+theme.addNoteList([REST] * 9 + cleanNotes * 3, [WN] * 9 + cleanNotesDurations * 3)
 
-  # Coro 16 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+# Añadir el tema al Part
+cleanGuitarPart.addPhrase(theme)
 
-  # Bridge PT1 8 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+# Bridge + Gtr. Solo 1
+chords_pitches = [[REST]] * 8
+chords_durations = [WN] * 8
 
-  # Bridge PT2 16 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+DURATIONS1 = [QN, EN, EN, EN, EN, EN, EN]
+DURATIONS2 = [EN, EN, EN, QN, EN, EN, EN]
 
-  # Break 8 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+# Definición de acordes
+CHORDS_1_1 = [[E4, E4, C4, G4, C4], [E4, E4, C4, G4, C4], [REST], [E4, C4, G4, C4], [E4, E4, C4, G4], [E4, C4, G4, C4], [F4, C4, A4, F4, C4, F4]]
 
-  # Chorus 16 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+CHORDS_1_2 = [[F4, C4, A4, F4, C4, F4], [F4, C4, A4, F4, C4, F4], [REST], [F4, C4, A4, F4, C4, F4], [C4, A4, F4, C4, F4], [F4, C4, A4, F4, C4], [C4, A4, F4, C4, F4]]
 
-  # Bridge PT1 8 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+CHORDS_1_3 = [[G4, D4, A4, G4, D4], [G4, D4, A4, G4, D4], [REST], [D4, A4, G4, D4], [G4, D4, A4, G4], [D4, A4, G4, D4], [G4, D4, B4, G4, D4, G4]]
 
-  # Bridge PT2 16 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+CHORDS_1_4 = [[G4, D4, B4, G4, D4, G4], [G4, D4, B4, G4, D4, G4], [REST], [G4, D4, B4, G4, D4, G4], [D4, B4, G4, D4, G4], [G4, D4, B4, G4, D4], [D4, B4, G4, D4, G4]]
 
-  # Chorus 16 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+CHORDS_1_5 = [[E4, C4, A4, E4, A4], [E4, C4, A4, E4, A4], [REST], [C4, A4, E4, A4], [E4, C4, A4, E4], [C4, A4, E4, A4], [E4, B4, G4, E4, B4, E4]]
 
-  # Final Chorus 9 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
+CHORDS_1_6 = [[E4, B4, G4, E4, B4, E4], [E4, B4, G4, E4, B4, E4], [REST], [E4, B4, G4, E4, B4, E4], [B4, G4, E4, B4, E4], [E4, B4, G4, E4, B4], [B4, G4, E4, B4, E4]]
 
-  # Silence 2 4
-  bassPitches   += [CLP, CLP, CLP, REST, REST, CLP, CLP, CLP, REST] * 2
-  bassDurations += [QN, EN, EN, HN, QN, EN, EN, EN, DHN] * 2
-  
+CHORDS_1_7 = [[E4, C4, A4, E4, A4], [E4, C4, A4, E4, A4], [REST], [C4, A4, E4, A4], [E4, C4, A4, E4], [C4, A4, E4, A4], [G4, D4, A4, G4, D4]]
 
-  bassDrumPhrase.addNoteList(bassPitches, bassDurations)
-  drumsPart.addPhrase(bassDrumPhrase)
+CHORDS_1_8 = [[G4, D4, A4, G4, D4], [G4, D4, A4, G4, D4], [REST], [G4, D4, A4, G4, D4], [D4, A4, G4, D4], [G4, D4, A4, G4], [D4, A4, G4, D4]]
 
-  # TODO This is only for testing instrument individually. Delete after finishing part.
-  score.addPart(drumsPart)
-  Play.midi(score)
+# Añadir acordes y duraciones
+add_chords(CHORDS_1_1, DURATIONS1, chords_pitches, chords_durations)
+add_chords(CHORDS_1_2, DURATIONS2, chords_pitches, chords_durations)
 
-  return drumsPart
+add_chords(CHORDS_1_3, DURATIONS1, chords_pitches, chords_durations)
+add_chords(CHORDS_1_4, DURATIONS2, chords_pitches, chords_durations)
+
+add_chords(CHORDS_1_5, DURATIONS1, chords_pitches, chords_durations)
+add_chords(CHORDS_1_6, DURATIONS2, chords_pitches, chords_durations)
+
+add_chords(CHORDS_1_7, DURATIONS1, chords_pitches, chords_durations)
+add_chords(CHORDS_1_8, DURATIONS2, chords_pitches, chords_durations)
+
+
+for pitches, duration in zip(chords_pitches, chords_durations):
+    phrase.addChord(pitches, duration)
+
+cleanGuitarPart.addPhrase(phrase)
+
+# Bridge p.2a
+bridgeNotes = [B4, G4, D4, B4, G4, D4, D4, A4, F4, D4, A4, F4, D4, D4, B4, G4, E4, B4, G4, E4, E4, F4, D4, B4, F4, E4, F4, E4]
+bridgeNotesDurations = [EN, EN, QN, EN, EN, EN, EN] * 3 + [EN, EN, EN, EN, QN, EN, EN]
+
+individual_phrase.addNoteList(bridgeNotes * 4, bridgeNotesDurations * 4)
+cleanGuitarPart.addPhrase(individual_phrase)
+
+# Visualización y reproducción
+View.sketch(cleanGuitarPart)
+Play.midi(cleanGuitarPart)
